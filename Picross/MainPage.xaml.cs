87f32020ad884f54fa87855ad81ad987fb763f18
QuantumJZ <sukiwhitex7@gmail.com>
2024-05-22 14:41:56 -0600

    Setup Grid / Hover Color Changes

--- Picross/MainPage.xaml.cs
+++ Picross/MainPage.xaml.cs
@@ -6,6 +6,33 @@
         {
             InitializeComponent();
         }
-    }
 
+        private void OnPointerEntered(object sender, PointerEventArgs e)
+        {
+            Button button = (Button)sender;
+            button.BackgroundColor = Color.FromArgb("#F7D358");
+            button.BorderColor = Color.FromArgb("#FF8000");
+        }
+
+        private void OnPointerExited(object sender, PointerEventArgs e)
+        {
+            Button button = (Button)sender;
+            button.BackgroundColor = Color.FromArgb("#FFFFFF");
+            button.BorderColor = Color.FromArgb("#000000");
+        }
+
+        private void GameButtonEntered(object sender, PointerEventArgs e)
+        {
+            Button button = (Button)sender;
+            button.BackgroundColor = Color.FromArgb("#92def7");
+            button.BorderColor = Color.FromArgb("#01A9DB");
+        }
+
+        private void GameButtonExited(object sender, PointerEventArgs e)
+        {
+            Button button = (Button)sender;
+            button.BackgroundColor = Color.FromArgb("#FFFFFF");
+            button.BorderColor = Color.FromArgb("#000000");
+        }
+    }
 }
