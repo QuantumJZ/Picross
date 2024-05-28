d26d3a0a4936d235ce6dd20858a9925f47ce85aa
QuantumJZ <sukiwhitex7@gmail.com>
2024-05-28 17:09:04 -0600

    Added Fullscreen on startup

--- Picross/MauiProgram.cs
+++ Picross/MauiProgram.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
 
 namespace Picross
 {
@@ -14,9 +20,31 @@ namespace Picross
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+            #if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(windowsLifecycleBuilder =>
+                {
+                    windowsLifecycleBuilder.OnWindowCreated(window =>
+                    {
+                        window.ExtendsContentIntoTitleBar = false;
+                        var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        var id = Win32Interop.GetWindowIdFromWindow(handle);
+                        var appWindow = AppWindow.GetFromWindowId(id);
+                        switch (appWindow.Presenter)
+                        {
+                            case OverlappedPresenter overlappedPresenter:
+                                overlappedPresenter.SetBorderAndTitleBar(true, true);
+                                overlappedPresenter.Maximize();
+                                break;
+                        }
+                    });
+                });
+            });
+            #endif
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
